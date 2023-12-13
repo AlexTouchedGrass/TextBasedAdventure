ac1ae9f4f43b88468b55d6e172ef49a34761e006
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-12-13 09:19:34 -0500

    Nice Too

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,7 +1,18 @@
+
+import java.util.Scanner;
 public class Gameplay {
     public static void main(String[] args) {
-        String p1FirstName = InputHelper.getNonZeroLenString(scan, "Enter Person 1's First Name:");
-        String p1lastName = InputHelper.getNonZeroLenString(scan, "Enter Person 1's Last Name:");
-        int p1Age = InputHelper.getRangedInt(scan, "Get Person 1's Age: [1-99]", 1, 99);
+        Scanner scan = new Scanner(System.in);
+
+        String pFirstName = InputHelper.getNonZeroLenString(scan, "Enter the Player's First Name:");
+        String pLastName = InputHelper.getNonZeroLenString(scan, "Enter the Player's Last Name:");
+
+        if (pFirstName.equalsIgnoreCase("chosen") && pLastName.equalsIgnoreCase("one")){
+            int p1Age = InputHelper.getRangedInt(scan, "Get the Age: [0,999]", 0, 999); //Fun
+        } else {
+            int p1Age = InputHelper.getRangedInt(scan, "Get Player's Age: [16-40]", 16, 50); //Realism
+        }
+
+
     }
 }
